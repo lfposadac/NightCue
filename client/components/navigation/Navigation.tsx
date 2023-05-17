7b02cdb6729646fe336acbb57506dc4f8b264948
jpcortesg <jpcortesg@eafit.edu.co>
2023-05-17 14:03:46 -0500

    Login

--- client/components/navigation/Navigation.tsx
+++ client/components/navigation/Navigation.tsx
@@ -1,36 +1,19 @@
-import Link from 'next/link'
-import styles from './Navigation.module.css'
-import { link } from '@/interfaces/link'
+"use client";
 
-const links: link[] = [
-  {
-    label: 'Home',
-    route: '/'
-  },
-  {
-    label: 'Login',
-    route: '/login'
-  },
-  {
-    label: 'Reserva',
-    route: '/reserva'
-  },
-  {
-    label: 'Post',
-    route: '/posts'
-  },
-  {
-    label: 'About',
-    route: '/about'
-  }
-]
+import Link from "next/link";
+import styles from "./Navigation.module.css";
+import { link } from "@/interfaces/link";
+import { links, linksWithToken } from "./data";
 
-export function Navigation () {
+export function Navigation() {
+  const token = localStorage.getItem("token");
+  const currentLinks = token ? linksWithToken : links;
+
   return (
     <header className={styles.header}>
       <nav className={styles.nav}>
         <ul className={styles.navigation}>
-          {links.map(({ label, route }) => (
+          {currentLinks.map(({ label, route }) => (
             <li className={styles.li} key={route}>
               <Link href={route}> {label} </Link>
             </li>
@@ -38,5 +21,5 @@ export function Navigation () {
         </ul>
       </nav>
     </header>
-  )
+  );
 }
